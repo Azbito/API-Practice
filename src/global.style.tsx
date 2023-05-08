3f18f2e8ec37f8e545340b9d042103316024cc9e
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-08 00:47:59 -0300

    FEAT: global style file

---
+++ src/global.style.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #181524;
+    font-family: 'Poppins', sans-serif;
+    color: white;
+    word-break: break-all;
+  }
+`
